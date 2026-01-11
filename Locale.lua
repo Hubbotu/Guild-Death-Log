2dd0aa9745c20dacdc1f6388972911b7e45961c4
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2026-01-11 07:48:46 +0300

    Add files via upload

---
+++ Locale.lua
@@ -0,0 +1,169 @@
+-- Translator ZamestoTV
+-- Classes
+L.CLASSES = {[1]="Воин",[2]="Паладин",[3]="Охотник",[4]="Разбойник",[5]="Жрец",[7]="Шаман",[8]="Маг",[9]="Чернокнижник",[11]="Друид"}
+
+-- Core
+L.ADDON_TITLE = "Книга Павших"
+L.ADDON_SHORT = "Книга"
+L.NO_GUILD = "Нет гильдии"
+L.NO_GUILD_INFO = "Вы не состоите в гильдии.\nВступите в гильдию, чтобы отслеживать смерти."
+L.UNKNOWN = "Неизвестно"
+L.LEVEL = "Уровень"
+L.OK = "ОК"
+L.CANCEL = "Отмена"
+L.CLOSE = "Закрыть"
+L.SAVE = "Сохранить"
+L.DELETE = "Удалить"
+L.EDIT = "Редактировать"
+L.BACK = "Назад"
+L.VERSION = "Версия"
+
+-- Statistics
+L.GUILD_DEATHS = "Павшие члены гильдии"
+L.TOTAL_DEATHS = "%d Павших"
+L.AVG_LEVEL = "Сред. ур.: %.1f"
+L.TODAY = "Сегодня: %d"
+L.THIS_WEEK = "Неделя: %d"
+L.FALLEN = "Павшие"
+L.FALLEN_TODAY = "Сегодня"
+L.FALLEN_WEEK = "На этой неделе"
+L.NO_LOSSES = "Нет потерь!\nГильдия стоит крепко."
+
+-- Buttons (UI)
+L.BTN_REFRESH = "Обновить"
+L.BTN_SETTINGS = "Настройки"
+L.BTN_SYNC = "Синх"
+L.BTN_EXPORT = "Экспорт"
+L.BTN_DEBUG = "Отладка"
+L.BTN_HALL_OF_FAME = "Зал Славы"
+L.BTN_STATISTICS = "Статистика"
+L.BTN_MILESTONES = "Вехи"
+L.BTN_PROFESSIONS = "Профессии"
+L.BTN_GUILD_MAP = "Карта гильдии"
+L.BTN_TITLES = "Титулы"
+L.BTN_MEMORIAL = "Мемориал"
+L.BTN_GUILD_STATS = "Статистика гильдии"
+L.BTN_RULES = "Правила"
+L.BTN_BOOK = "Книга"
+
+-- Guild Rules
+L.RULES_TITLE = "Правила гильдии"
+L.RULES_CAN_EDIT = "Вы можете редактировать правила"
+L.RULES_CANNOT_EDIT = "Только офицеры могут редактировать правила"
+L.RULES_UPDATED = "Правила гильдии обновлены! Пожалуйста, прочитайте."
+L.RULES_SAVED = "Правила гильдии сохранены и распространены!"
+L.RULES_REQUESTING = "Запрашиваем правила у членов гильдии..."
+L.RULES_ONLY_OFFICERS = "Только офицеры могут редактировать правила!"
+L.RULES_CHAT_UPDATED = "[Правила гильдии] Обновлены, пожалуйста прочитайте! (/gdl rules)"
+
+-- Settings
+L.SETTING_ANNOUNCE = "Объявление в чат гильдии"
+L.SETTING_SOUND = "Проиграть звук"
+L.SETTING_OVERLAY = "Показывать всплывающее окно"
+L.SETTING_MAP_MARKERS = "Метки на карте"
+L.SETTING_DEBUG = "Отладочный вывод"
+L.SETTING_BLIZZARD_CHANNEL = "Канал Blizzard HC"
+L.SETTING_ADDON_CHANNEL = "Синхронизация аддона"
+L.SETTING_GUILD_TRACKER = "Трекер гильдии"
+
+-- Sync & Status
+L.LIVE = "LIVE"
+L.SYNCED = "СИНХ"
+L.SYNC_REQUESTED = "Синхронизация запрошена..."
+L.SYNC_RECEIVED = "Синх: %s (ур. %d)"
+L.GUILD_CHANGED = "Гильдия изменена на: %s"
+L.GUILD_LEFT = "Вы покинули гильдию."
+
+-- Deaths
+L.DEATH_RECEIVED = "%s пал(ла)!"
+L.MAP_DIED = "Погиб: %s"
+L.KILLED_BY = "Убит"
+L.LAST_WORDS = "Последние слова"
+L.ZONE = "Зона"
+L.DATE = "Дата"
+L.TIME = "Время"
+
+-- Debug
+L.DEBUG_TITLE = "Отладка и информация о синхронизации"
+L.DEBUG_SYNC_STATUS = "Статус синхронизации"
+L.DEBUG_USERS = "Пользователи аддона: %d"
+L.DEBUG_LAST_SYNC = "Последняя синхр.: %s"
+L.DEBUG_DEATHLOG = "Статус журнала смертей"
+L.DEBUG_FOUND = "Найдено: %s"
+L.DEBUG_ENTRIES = "Записи: %d"
+L.DEBUG_GUILD_DEATHS = "Смерти гильдии: %d"
+L.DEBUG_LOG = "Журнал активности"
+L.DEBUG_CLEAR = "Очистить"
+L.DEBUG_NEVER = "Никогда"
+
+-- Hall of Fame
+L.HOF_TITLE = "Зал Славы"
+L.HOF_SUBTITLE = "Бессмертные"
+L.HOF_EMPTY = "Пока нет выживших 60 уровня"
+L.HOF_REACHED = "достиг 60 уровня"
+
+-- Memorial
+L.MEMORIAL_TITLE = "Зал Памяти"
+L.MEMORIAL_SUBTITLE = "Мы помним павших"
+L.MEMORIAL_EMPTY = "Нет павших, кого помнить"
+L.MEMORIAL_RIP = "Покойтесь с миром"
+
+-- Milestones
+L.MILESTONES_TITLE = "Вехи"
+L.MILESTONES_SUBTITLE = "Достижения выживания"
+L.MILESTONE_UNLOCKED = "Веха достигнута!"
+L.MILESTONE_LEVEL = "Достигнут уровень %d"
+L.MILESTONE_BOSS = "Побежден %s"
+L.MILESTONE_PROFESSION = "Навык %s %d"
+
+-- Professions
+L.PROFESSIONS_TITLE = "Профессии гильдии"
+L.PROFESSIONS_SUBTITLE = "Кто чем может заниматься?"
+L.PROFESSIONS_EMPTY = "Нет данных о профессиях"
+L.PROFESSIONS_SEARCH = "Поиск..."
+
+-- Titles
+L.TITLES_TITLE = "Титулы"
+L.TITLES_CURRENT = "Текущий титул:"
+L.TITLES_UNLOCKED = "титулов открыто"
+L.TITLES_CLICK_SELECT = "Кликните для выбора"
+L.TITLES_ACTIVE = "АКТИВЕН"
+L.TITLES_REQUIRED = "Требует:"
+L.TITLES_PRIORITY = "Приоритет:"
+
+-- Guild Stats
+L.GSTATS_TITLE = "Статистика гильдии"
+L.GSTATS_MEMBERS = "Члены"
+L.GSTATS_ONLINE = "Онлайн"
+L.GSTATS_AVG_LEVEL = "Средний уровень"
+L.GSTATS_DEATHS_TODAY = "Смертей сегодня"
+L.GSTATS_DEATHS_WEEK = "Смертей на неделе"
+L.GSTATS_DEATHS_TOTAL = "Всего смертей"
+
+-- Export
+L.EXPORT_TITLE = "Экспорт"
+L.EXPORT_DISCORD = "Формат Discord"
+L.EXPORT_TEXT = "Текстовый формат"
+L.EXPORT_CSV = "Формат CSV"
+L.EXPORT_STATS = "Статистика"
+L.EXPORT_COPY = "Выделите текст и Ctrl+C"
+
+-- Chronicle (main list)
+L.CHRONICLE_HEADER = "- Летопись Павших -"
+L.CHRONICLE_EMPTY = "Пока нет записей о павших"
+
+-- Quotes
+L.BOOK_QUOTES = {
+    "Их имена высечены в камне.",
+    "Пусть их души обретут покой.",
+    "Пали с честью.",
+    "Они никогда не будут забыты.",
+    "Герои не умирают, они живут в наших сердцах.",
+}
+
+-- Death Messages (simple versions for chat)
+L.DEATH_MESSAGES = {
+    "%s (ур. %d %s) пал(ла). Покойся с миром.",
+    "Гильдия оплакивает %s - Уровень %d %s",
+}
+end
